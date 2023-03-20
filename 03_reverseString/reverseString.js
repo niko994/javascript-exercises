7e920326819d4bb9e1d85cc0e6652ee1ec5164d5
niko994 <nicolasdominguez72@gmail.com>
2023-03-20 20:54:13 -0300

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,24 +1,6 @@
 let reverseString = function(word) {
-    let string = word.split('');
-    let stringLenght = 
-    return string[string.length-1] + string[string.length-2] + string[string.length-3] + string[string.length-4] + string[string.length-5] 
-}
-
-
-
-
-/*let reverseString = function(word) {
-    let string = word.split('');
-    return string[string.length-1] + string[string.length-2] + string[string.length-3] + string[string.length-4] + string[string.length-5] 
-}*/
-
-
-/*function reverseString (word) {
-let string = word.split('');
-    return string[string.length-1] + string[string.length-2] + string[string.length-3] + string[string.length-4] + string[string.length-5]    
-}
-
-console.log(reverseString("hello"))*/
+    return word.split('').reverse().join('');
+}     
 
 // Do not edit below this line
 module.exports = reverseString;
