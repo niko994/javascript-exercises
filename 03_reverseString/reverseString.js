40b78f81795e47ce2d48d7da034035d81ec2fa43
niko994 <nicolasdominguez72@gmail.com>
2023-03-20 20:12:39 -0300

    work on exercise number 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+let reverseString = function(word) {
+    let string = word.split('');
+    let stringLenght = 
+    return string[string.length-1] + string[string.length-2] + string[string.length-3] + string[string.length-4] + string[string.length-5] 
+}
 
-};
+
+
+
+/*let reverseString = function(word) {
+    let string = word.split('');
+    return string[string.length-1] + string[string.length-2] + string[string.length-3] + string[string.length-4] + string[string.length-5] 
+}*/
+
+
+/*function reverseString (word) {
+let string = word.split('');
+    return string[string.length-1] + string[string.length-2] + string[string.length-3] + string[string.length-4] + string[string.length-5]    
+}
+
+console.log(reverseString("hello"))*/
 
 // Do not edit below this line
 module.exports = reverseString;
