3aac19214726d02990e8d164b605eba8eac912fa
niko994 <nicolasdominguez72@gmail.com>
2023-03-20 21:25:34 -0300

    Work on 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, position, ) {
+    let newArray = array.splice(position - 1, 1);
+    return array
 };
 
 // Do not edit below this line
